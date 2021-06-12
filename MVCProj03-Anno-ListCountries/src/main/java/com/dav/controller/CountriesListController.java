ab128ca739bff6aa1be988cc6b8b91c46d48c049
david <david@david-Vostro-15-3568>
2021-06-12 21:08:22 +0530

    One Handler class with different possible signatures of Handler methods.

--- MVCProj03-Anno-ListCountries/src/main/java/com/dav/controller/CountriesListController.java
+++ MVCProj03-Anno-ListCountries/src/main/java/com/dav/controller/CountriesListController.java
@@ -1,13 +1,12 @@
 package com.dav.controller;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.dav.service.ICountriesMgmtService;
 
@@ -16,35 +15,109 @@ public class CountriesListController {
 
 	@Autowired
 	public ICountriesMgmtService service;
+
+	/*  
+	 * DIFFERENT KINDS OF SIGNATURE FOR HANDLER METHOD
+	 * 
+	 * @RequestMapping("/list_countries") public ModelAndView fetchCountries() {
+	 * 
+	 * List<String> countriesList = service.getCountries(); // create MAV object
+	 * ModelAndView mav = new ModelAndView(); // add multiple model Attributes to
+	 * mav mav.addObject("countriesInfo", countriesList); mav.addObject("sysDate",
+	 * LocalDateTime.now()); // set Logical View Name to mav Object
+	 * mav.setViewName("countriesList"); // return MAV object return mav; }
+	 */
+
+	// Best Signature for Handler method (makes Handler method Invasive with Map)
+	@RequestMapping("/list_countries")
+	public String fetchCountries(Map<String, Object> map) {
+
+		List<String> countriesList = service.getCountries();
+		map.put("countriesInfo", countriesList);
+		map.put("sysDate", LocalDateTime.now());
+		return "countriesList";
+	}
+
+	/*
+	// If the retrunType is Void it takes income request URI as the Logical View Name(list_countires)
+	@RequestMapping("/list_countries")
+	public void fetchCountries(Map<String, Object> map) {
+
+		List<String> countriesList = service.getCountries();
+		map.put("countriesInfo", countriesList);
+		map.put("sysDate", LocalDateTime.now());
+	}
+	*/
+
+	/*
+	//Internally DS creates Obj for Model which is super Interface of BindingAwareModelMap(actual obj)
+	
+	@RequestMapping("/list_countries")
+	public String fetchCountries(Model model) {
+
+		List<String> countriesList = service.getCountries();
+		
+		model.addAttribute("countriesInfo", countriesList);
+		model.addAttribute("sysDate", LocalDateTime.now());
+		
+		return "countriesList";
+	}
+	*/
+	/*
+	//using ModelMap ref for the BindingAwareModelMap object
+	@RequestMapping("/list_countries")
+	public String fetchCountries(ModelMap map) {
+
+		List<String> countriesList = service.getCountries();
+		map.put("countriesInfo", countriesList);
+		map.put("sysDate", LocalDateTime.now());
+		return "countriesList";
+	}
+	*/
+	
+	/*
+	//Here ret. type is model so we have to explicitly create the object and add attributes to it
+	@RequestMapping("/list_countries")
+	public ModelMap fetchCountries() {
+
+		List<String> countriesList = service.getCountries();
+		//create ModelMap Obejct
+		ModelMap modelMap = new BindingAwareModelMap();
+		modelMap.put("countriesInfo", countriesList);
+		modelMap.put("sysDate", LocalDateTime.now());
+		//return ModelMap
+		return modelMap;
+	}
+	
+	*/
+	/*
+	//ModelAndView is not accepted as argument in Handler Method . now no Data is being fetched to view
+	@RequestMapping("/list_countries")
+	public String fetchCountries(ModelAndView mav) {
+
+		List<String> countriesList = service.getCountries();
+		mav.addObject("countriesInfo", countriesList);
+		mav.addObject("sysDate", LocalDateTime.now());
+		return "countriesList";
+	}
+	
+	*/
+	
 	
 	
 	@RequestMapping("/welcome")
 	public String showHomePage() {
-		return "home" ;
+		return "home";
 	}
-	
+
 	@RequestMapping("/about_us")
 	public String showAboutUsPage() {
 		return "aboutUs";
 	}
-	
+
 	@RequestMapping("/contact_us")
 	public String showContactUsPage() {
 		return "contactUs";
 	}
-	
-	@RequestMapping("/list_countries")
-	public ModelAndView fetchCountries() {
-		
-		List<String> countriesList = service.getCountries();
-		//create MAV object
-		ModelAndView mav = new ModelAndView();
-		//add multiple model Attributes to mav obj
-		mav.addObject("countriesInfo",countriesList);
-		mav.addObject("sysDate",LocalDateTime.now());
-		// set Logical View Name to mav Object
-		mav.setViewName("countriesList");
-		//return MAV object
-		return mav;
-	}
+
 }
---
+++ MVCProj03-Anno-ListCountries/src/main/webapp/WEB-INF/pages/list_countries.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" isELIgnored="false"%>
+    
+    <b>list_countries.jsp</b>
+
+    <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+    <center>
+    <h2 style="color:red;text-align:center;">Date and Time :: ${sysDate }</h2>
+    <c:choose>
+    	<c:when test="${!empty countriesInfo}" >
+    	<table border="2 red;">
+    	<tr><th>Sr.No</th><th>COUNTRY</th></tr>
+			<c:forEach varStatus="SrNo"  var="country" items="${countriesInfo}">
+							<tr><td>${SrNo.count }</td>
+							<td><b>${country }</b></td></tr>
+			</c:forEach>
+			</table>
+    	</c:when>
+    	
+    	<c:otherwise>
+    		<h1 style="color:red;text-align:center;">Data Could not be Fetched</h1>
+    	</c:otherwise>
+    </c:choose>
+    
+    <b>Model Data :</b><br> "sysDate} :(checks data in all the
+	scopes) ::" ${sysDate } <br> "requestScope.sysDate - checks in
+	only reqeustScope]"${requestScope.sysDate } <br> " Expression tag
+	based retrieving data using request.getAttribute('sysDate')::"
+	<%=request.getAttribute("sysDate")%>
+	
+    <br>
+    <a href="welcome">HOME</a>
+    </center>
